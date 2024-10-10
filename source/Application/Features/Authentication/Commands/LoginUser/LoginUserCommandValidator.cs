fc275db23aa31ca8a6c879333b17ab54c6c17708
Lucas Cruz <lucascruzestudo@gmail.com>
2024-10-10 00:42:56 -0300

    fix: improving validation of request parameters

--- source/Application/Features/Authentication/Commands/LoginUser/LoginUserCommandValidator.cs
+++ source/Application/Features/Authentication/Commands/LoginUser/LoginUserCommandValidator.cs
@@ -5,19 +5,16 @@ public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
     public LoginUserCommandValidator()
     {
         RuleFor(x => x.Request)
-            .NotNull().WithMessage("{PropertyName} is required.");
-
-        RuleFor(x => x.Request.Login)
-            .NotEmpty().WithMessage("{PropertyName} is required.")
-            .When(x => x.Request != null)
             .NotNull().WithMessage("{PropertyName} is required.")
-            .When(x => x.Request != null);
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Request.Login)
+                    .NotEmpty().WithMessage("{PropertyName} is required.")
+                    .NotNull().WithMessage("{PropertyName} is required.");
 
-        RuleFor(x => x.Request.Password)
-            .NotEmpty().WithMessage("{PropertyName} is required.")
-            .When(x => x.Request != null)
-            .NotNull().WithMessage("{PropertyName} is required.")
-            .When(x => x.Request != null);
-
+                RuleFor(x => x.Request.Password)
+                    .NotEmpty().WithMessage("{PropertyName} is required.")
+                    .NotNull().WithMessage("{PropertyName} is required.");
+            });
     }
 }
--- source/Application/Features/Authentication/Commands/RegisterUser/RegisterUserCommandRequest.cs
+++ source/Application/Features/Authentication/Commands/RegisterUser/RegisterUserCommandRequest.cs
@@ -2,7 +2,7 @@ namespace Project.Application.Features.Commands.RegisterUser;
 
 public record RegisterUserCommandRequest
 {
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public required string Email { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
